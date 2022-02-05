6564750fe9f6cef281f1d223d09be2e52d99169c
mrOctopus001 <saefulanwaroktariansah@gmail.com>
2022-02-05 14:36:37 +0700

    test

--- lib/src/views/detail_group/detailGroup.dart
+++ lib/src/views/detail_group/detailGroup.dart
@@ -118,7 +118,7 @@ class _CarddetailState extends State<Carddetail> {
                             padding: EdgeInsets.only(top: 4),
                           ),
                           Text(
-                            'Loremm',
+                            'Loremmtfvtvgtbgy',
                             style: TextStyle(
                               color: Colors.white,
                             ),
